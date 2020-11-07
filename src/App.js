6fb833399042e901631562c7ffee05b586b756c5
EdonJung <2donny@naver.com>
2020-11-07 09:31:43 +0900

    SurveyContents router started [State setting]

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Layout from './components/Layout/Layout';
 import { Switch, Route } from 'react-router-dom';
 import SurveyStart from './components/SurveyStart/SurveyStart';
+import SurveyContents from './containers/SurveyContents/SurveyContents';
 
 class App extends React.Component {
   render() {
@@ -10,7 +11,7 @@ class App extends React.Component {
       <div className="App">
         <Layout>
           <Switch>
-            <Route path='/survey/1/a' />
+            <Route path='/survey/1/a' component={SurveyContents}/>
             <Route path='/survey/1/b' />
             <Route path='/survey/1/c' />
 
--- src/containers/SurveyContents/SurveyContents.js
+++ src/containers/SurveyContents/SurveyContents.js
@@ -8,16 +8,26 @@ import SurveyErrormsg from './SurveyErrormsg/SurveyErrormsg';
 
 class SurveyContents extends React.Component {
     state = {
-        survey_1: {
+        currentStatus: {
+            currentCategoryNum: 1,
+            currentQuestionNum: 1,
+            currentProgress: '3%',
+        },
+        survey_1: { //노인장기요양보험 가능여부
+            name: '',
+            gender: '',
             age: '',
             hasDisease: null,
-            status: ''
+            statusNumber: null, //거동이 어려워 누워계시다면 1단계, 이런식으로 ~
+        }, 
+        survey_2: { //보호자 가능 여부
+            isSpouse: null, // 배우자이면 true로 
+            isFamily: null, // 배우자가 아닌 가족이면 true로 
+            // 둘다 nul이면 기타니까 불가능을 리턴
         },
-        survey_2: {
-            hasRelationship: null
-        },
-        survey_3: {
-            
+        survey_3: { //최대금액
+            workMorethan160: null,
+            hasViolence: null, //폭력성이 있는가
         }
     }
     render() {
